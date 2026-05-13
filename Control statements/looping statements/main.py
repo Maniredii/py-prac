e8d54e8d35846a500e2cedbaa24fdeaa05363058
Maniredii <sivareddyevuri92@gmail.com>
2026-05-13 11:02:23 +0530

    intitla commit

---
+++ Control statements/looping statements/main.py
@@ -0,0 +1,22 @@
+#----------find the factorial of a number
+
+# i = int(input("enter your num : "))
+# fact = 1
+# while i > 1: #here 5 will be the value of i and it will be decremented by 1 until it becomes 1
+#     fact = fact*i 
+#     i = i - 1 #here i value will be decremented by 1 until it becomes 1
+# print(fact)
+
+#-------- sum of numeric values of a cell
+# address each value from the cell
+# check wether the value is numeric or not
+#if numeric add the numeric value to with existing sum
+
+coll = eval(input("enter your collection : "))
+add = 0
+i = 0
+while i < len(coll):
+    if type(coll[i]) in [int, float, complex]:
+        add = add+coll[i]
+    i = i+ 1
+print(add)
